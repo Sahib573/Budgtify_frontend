6a4304237a48dc21ad42bb35b8b928c5600455d3
Sahib Singh <76395667+Sahib573@users.noreply.github.com>
2023-12-11 04:41:15 +0530

    Delete backend directory

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express"); //fetch the already present instance of express
-const router = express.Router();
-// const passport = require("passport");
-//adding controller
-
-router.use("/user", require("./user"));
-router.use("/expense", require("./expense"));
-router.use("/group",require("./group"));
-module.exports = router;
